e23952617a9184c3a2a603ff95cb5ca01f94f279
AntonDavydov <daver1107@yahoo.com>
2021-08-18 18:40:11 -0700

    checkout action added

--- src/test/java/Sample.java
+++ src/test/java/Sample.java
@@ -1,9 +1,9 @@
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -12,13 +12,13 @@ public class Sample {
     private static final String xpathAddToCartElement = "/html/body/div/div/div/main/div/div/section/ul/li[1]/div/div[4]/form/div/div/div[1]/input";
     private static final String xpathGoToCartElement = "/html/body/section/div[1]/div/div/div[2]/div/div/a";
     private static final String xpathChekOutElement = "//input[@name= \"checkout\"]";
-    public RemoteWebDriver dr;
+    private RemoteWebDriver dr;
     private static final String URL = "http://localhost:4444/wd/hub/";
     private static final String testURL = "http://www.dogspecial.ru";
 
     static void sleep() {
         try {
-            Thread.sleep(200);
+            Thread.sleep(600);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -26,7 +26,7 @@ public class Sample {
 
     @BeforeTest
     void start() throws MalformedURLException {
-        this.dr = new RemoteWebDriver(new URL(URL), new ChromeOptions());
+        dr = new RemoteWebDriver(new URL(URL), new ChromeOptions());
     }
 
     @Test
@@ -40,15 +40,47 @@ public class Sample {
         dr.findElementByXPath(xpathGoToCartElement).click();
         sleep();
         dr.findElementByXPath(xpathChekOutElement).click();
+
         sleep();
+        //Кэшируем поля формы оформления заказа
+        WebElement firstName = dr.findElementByXPath("//input [@name = \"customer[firstname]\"]");
+        WebElement lastName = dr.findElementByXPath("//input [@name = \"customer[lastname]\"]");
+        WebElement phone = dr.findElementByXPath("//input [@name = \"customer[phone]\"]");
+        WebElement email = dr.findElementByXPath("//input [@name = \"customer[email]\"]");
+        WebElement street = dr.findElementByXPath("//input [@name = \"customer[address.shipping][street]\"]");
+        WebElement city = dr.findElementByXPath("//input [@name = \"customer[address.shipping][city]\"]");
+        WebElement region = dr.findElementByXPath("//*/select/option[43]");
+        WebElement zip = dr.findElementByXPath("//input [@name = \"customer[address.shipping][zip]\"]");
+        //Заполняем поля данными
+        firstName.click();
+        firstName.sendKeys("Ivan");
+        lastName.click();
+        lastName.sendKeys("Ivanov");
+        phone.click();
+        phone.sendKeys("4951234567");
+        email.click();
+        email.sendKeys("ivan@ivanov.com");
+        street.click();
+        street.sendKeys("123 Main street");
+        city.click();
+        city.sendKeys("Moscow");
+        region.click();
+        sleep();
+        region.click();
+        sleep();
+        zip.click();
+        zip.sendKeys("12345");
+        sleep();
+        //Отправляем заказ
+        dr.findElementByXPath("//input [@class= \"s-step-submit\"]").click();
         sleep();
 
     }
 
-    @AfterTest
-    void close(){
-        dr.close();
-        dr.quit();
-    }
+//    @AfterTest
+//    void close(){
+//        dr.close();
+//        dr.quit();
+//    }
 
 }
